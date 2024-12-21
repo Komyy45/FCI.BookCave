065e12071657db9efe3a2b87c075fa3fbd83d76b
AFIFY <ahmedhisham8.ah@gmail.com>
2024-12-21 09:58:12 +0200

    Send Email

---
+++ FCI.BookCave.Dashboard/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using FCI.BookCave.Dashboard.Email;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCI.BookCave.Dashboard.Controllers
+{
+    public class EmailController : Controller
+    {
+        private IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task<ActionResult> sendEmail()
+        {
+            return View("sendEmail");
+        }
+
+        [HttpPost]
+        public async Task <ActionResult> sendEmail(EmailViewModel email)
+        {
+            await _emailService.sendAsync("ahmedhisham8.ah@gmail.com","Rawash BYMSI","Helloz Afify");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
