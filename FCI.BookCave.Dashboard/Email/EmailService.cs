f3d86893a7d1960f4ee6856630f9e2ea4f41fc3e
Ahmed Hisham <ahmedhisham8.ah@gmail.com>
2024-12-22 22:32:46 +0200

    Add Feedback

--- FCI.BookCave.Dashboard/Email/EmailService.cs
+++ FCI.BookCave.Dashboard/Email/EmailService.cs
@@ -10,14 +10,18 @@ namespace FCI.BookCave.Dashboard.Email
             string mail = "gwork1233@outlook.com";
             string pass = "Password123##";
 
-            var client = new SmtpClient("smtp.office365.com", 587)
-            {
-                EnableSsl = true,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(mail, pass)
-            };
-
-            return client.SendMailAsync(
+            //var client = new SmtpClient("smtp.office365.com", 587)
+            //{
+            //    EnableSsl = true,
+            //    UseDefaultCredentials = false,
+            //    Credentials = new NetworkCredential(mail, pass)
+            //};
+            SmtpClient smtp = new SmtpClient();
+            smtp.UseDefaultCredentials = false;
+            smtp.Host = "smtp.office365.com";
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential(mail, pass);
+            return smtp.SendMailAsync(
             new MailMessage(from: "gwork1233@outlook.com",
                             to: email,
                             subject,
